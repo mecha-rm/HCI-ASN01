34f6802e71fffa826586fe2a7f5bc2d1011ac832
Roderick Montague <roderick.montague243@gmail.com>
2021-09-18 19:53:55 -0400

    Base Project
    Base project for assignment 1 of Human-Computer Interactions.

---
+++ HCI-ASN01.R
@@ -0,0 +1,5 @@
+# Names: Adam Kahil, Eric Aivaliotis, Hao Tian Guan, and Roderick "R.J." Montague
+# Description:
+# References:
+
+# INFR 4350U - Human-Computer Interactions for Games - Assignment 1
